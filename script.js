a3c6b4ab8ecf8f59bb42cfaab592ea851cdbc1d2
Raymond Lyon <ralapple@thelyonfamily.net>
2022-09-29 22:12:02 +0000

    added ability to grab current location and turn that to coordinates

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 var test = document.getElementById("test")
+if (!navigator.geolocation) {
+    console.error(`Your browser doesn't support Geolocation`);
+}
+var location = navigator.geolocation.getCurrentPosition(onSuccess, onError)
+
+function onSuccess(position){
+  const{
+    latitude,
+    longitude
+  } = position.coords
+  console.log(latitude)
+  console.log(longitude)
+}
+
+
+function onError(){
+  console.log("Error")
+}
 document.getElementById('button').onclick = function() {
   console.log("Clicked")
   var city = document.getElementById('city').value
