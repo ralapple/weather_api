6a34ee19fbfbe2bd40a08f4b2f25fb821122401a
Raymond Lyon <ralapple@thelyonfamily.net>
2022-09-30 02:24:15 +0000

    New version

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-var test = document.getElementById("test")
+
+
+
 if (!navigator.geolocation) {
     console.error(`Your browser doesn't support Geolocation`);
 }
-var location = navigator.geolocation.getCurrentPosition(onSuccess, onError)
-
 function onSuccess(position){
   const{
     latitude,
     longitude
   } = position.coords
-  console.log(latitude)
-  console.log(longitude)
+  var lat = latitude
+  var long = longitude
+  //console.log(latitude)
+  //console.log(longitude)
+  getWeather(lat,long)
 }
-
 
 function onError(){
   console.log("Error")
 }
+
+
+
 document.getElementById('button').onclick = function() {
   console.log("Clicked")
-  var city = document.getElementById('city').value
-  var state = document.getElementById('state').value
-
-  city = 44.9778
-  state = -93.2650
-  console.log(city + ", " + state)
-  fetch('https://api.weather.gov/points/'+city+','+state)
-    .then(resp => resp.json())
-    .then((json) => console.log(json))
+  navigator.geolocation.getCurrentPosition(onSuccess, onError)
 }
 
-console.log()
+function getWeather(lat, long){
+  console.log(lat)
+  console.log(long)
+  document.getElementById('lat').innerHTML = lat
+  document.getElementById('long').innerHTML = long
+  fetch('https://api.weather.gov/points/'+lat+','+long)
+    .then(resp => resp.json())
+    .then((json) => console.log(json))
+  
+}
