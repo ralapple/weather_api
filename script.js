ea7cb194e9cf3b48ea352cf0b00b4a0b9c24e24c
Raymond Lyon <ralapple@thelyonfamily.net>
2022-11-21 21:14:20 +0000

    5 day temps now visable

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 if (!navigator.geolocation) {
   console.error(`Your browser doesn't support Geolocation`);
 }
@@ -22,7 +25,7 @@ function getWeather() {
   })
   .then(function (data) {
     //console.log(data)
-    console.log(data.city);
+    //console.log(data.city);
     var city = data.city
     var state = data.principalSubdivision
     document.getElementById('location').innerHTML = city + ", " + state
@@ -33,28 +36,54 @@ function getWeather() {
   
   
 
-  fetch('https://api.weather.gov/gridpoints/MPX/108,71/forecast/hourly')
+  fetch('https://api.weather.gov/gridpoints/MPX/108,71/forecast')
   .then(function (response) {
     return response.json();
   })
-  .then(function (data1) {
-    var temp = data1.properties.periods[0].temperature
-    var forecast = data1.properties.periods[0].shortForecast
-    var windSpeed = data1.properties.periods[0].windSpeed
-    var windDirection = data1.properties.periods[0].windDirection
 
-    var str = temp +"F, "+ forecast + ", Wind:" + windSpeed + " " +windDirection
-    if (str == null){
-      document.getElementById('temp').innerHTML = null
-    } else{
-         document.getElementById('temp').innerHTML = str
+    
+  .then(function (data1){
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    for (let i = 0; i< 12; i += 2)
+    {
+      var temp = data1.properties.periods[i].temperature
+      var forecast = data1.properties.periods[i].shortForecast
+      var windSpeed = data1.properties.periods[i].windSpeed
+      var windDirection = data1.properties.periods[i].windDirection
+      if (i == 0){
+        var str = temp +"F, "+ forecast + ", Wind:" + windSpeed + " " +windDirection
+      }
+      else{
+        var str = temp; 
+      }
+      if (str == null){
+      document.getElementById('t'+(i/2)).innerHTML = null
+      } else{
+         document.getElementById('t'+(i/2)).innerHTML = str
     }
-    console.log(temp, forecast, windSpeed, windDirection)
+      
+    }
+    // var temp = data1.properties.periods[0].temperature
+    // var forecast = data1.properties.periods[0].shortForecast
+    // var windSpeed = data1.properties.periods[0].windSpeed
+    // var windDirection = data1.properties.periods[0].windDirection
+
+    // var str = temp +"F, "+ forecast + ", Wind:" + windSpeed + " " +windDirection
+    // if (str == null){
+    //   document.getElementById('temp').innerHTML = null
+    // } else{
+    //      document.getElementById('temp').innerHTML = str
+    // }
+    // console.log(temp, forecast, windSpeed, windDirection)
+    // console.log("hello")
+    // console.log(data1.properties.periods.length)
 
   })
   .catch(function (err) {
     console.log(err);
   });
+
+  
 
 
 
